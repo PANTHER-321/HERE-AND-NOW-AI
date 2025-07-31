15de960cab50cab03e8fb80bf40daa4f5ebe08a8
PANTHER-321 <panther96312@gmail.com>
2025-07-31 12:43:06 +0530

    My latest changes

--- project-5-hello-langchain/hello_langchain.py
+++ project-5-hello-langchain/hello_langchain.py
@@ -4,3 +4,11 @@ import os
 
 load_dotenv()
 goole_api_key = os.getenv("GOOGLE_API_KEY")
+model ="gemini-2.5-flash-lite"
+
+def run_hello_langchain():
+    llm = chatGoogleGenerativeAI(model=model, api_key=goole_api_key)
+    response = llm.invoke("explain RLHF in 2 lines!")
+    print(response.content)
+
+run_hello_langchain()    
